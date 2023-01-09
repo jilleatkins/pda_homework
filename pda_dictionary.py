19b42471fffc01b75ef195bfe15739b7391a238d
jilleatkins <jilleatkins@gmail.com>
2023-01-09 16:10:23 +0000

    Initial commit

---
+++ pda_dictionary.py
@@ -0,0 +1,6 @@
+my_dictionary = {
+  "name": "Pinky",
+  "career": "Serial cuddler",
+  "age": 3,
+  "colour": "brown"}
+print(my_dictionary)
---
+++ pda_list.py
@@ -0,0 +1,3 @@
+task_list = ['brush teeth', 'make dinner', 'fill dog water bowl']
+task_list.append('clean toilet')
+print(task_list)
